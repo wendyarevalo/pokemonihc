b5985c1960fef1bfe1fe27b12924a2fb0161f019
wendyarevalo <33068924+wendyarevalo@users.noreply.github.com>
2018-10-23 14:24:11 -0500

    Subiendo proyecto pokemon

---
+++ DBTest/Assets/dataInsert.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dataInsert : MonoBehaviour {
+
+    public string inputUserName;
+    public string inputPassword;
+    public string inputEmail;
+
+    string createUserURL = "http://localhost/db_test/insertUser.php";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CreateUser(inputUserName, inputPassword, inputEmail);
+        }
+		
+	}
+
+    public void CreateUser(string username, string password, string email) {
+        WWWForm form = new WWWForm();
+        form.AddField("userPost",username);
+        form.AddField("passPost", password);
+        form.AddField("emailPost", email);
+
+        WWW www = new WWW(createUserURL,form);
+    }
+}
---
+++ DBTest/Assets/login.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class login : MonoBehaviour {
+
+    public InputField inputUsername;
+    public InputField inputPassword;
+    
+
+    string loginUserURL = "http://localhost/db_test/loginUser.php";
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+           StartCoroutine(LoginToDB(inputUsername.text, inputPassword.text));
+        }
+    }
+
+    public void Logear() {
+        StartCoroutine(LoginToDB(inputUsername.text, inputPassword.text));
+    }
+
+    IEnumerator LoginToDB(string username, string password) {
+        WWWForm form = new WWWForm();
+        form.AddField("userPost", username);
+        form.AddField("passPost", password);
+
+        WWW www = new WWW(loginUserURL, form);
+
+        yield return www;
+
+        Debug.Log(www.text);
+    }
+}
