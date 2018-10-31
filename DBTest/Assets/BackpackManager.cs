422e303fc754477571d85434a6a9d8e25e53e97a
wendyarevalo <33068924+wendyarevalo@users.noreply.github.com>
2018-10-30 23:16:55 -0600

    Agregando Colisión Mochila-Pony

---
+++ DBTest/Assets/BackpackManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackpackManager : MonoBehaviour
+{
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Pony")
+        {
+            StartCoroutine("CatchPony", other.gameObject);
+        }
+
+    }
+    IEnumerator CatchPony(GameObject Pony)
+    {
+        /*transform.Translate(Vector3.up * 1, Space.World);
+        this.GetComponent<Rigidbody>().useGravity = false;
+        this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        Destroy(Pony.gameObject);*/
+        return null;
+    }
+}
---
+++ DBTest/Assets/DragAndThrow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndThrow : MonoBehaviour
+{
+
+    bool draging = false;
+    float distance;
+    public float ThrowSpeed;
+    public float ArchSpeed;
+    public float Speed;
+
+    private void OnMouseDown()
+    {
+        distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        draging = true;
+    }
+
+    public void OnMouseUp()
+    {
+        this.GetComponent<Rigidbody>().useGravity = true;
+        this.GetComponent<Rigidbody>().velocity += this.transform.forward * ThrowSpeed;
+        this.GetComponent<Rigidbody>().velocity += this.transform.up * ArchSpeed;
+        draging = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (draging)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Vector3 rayPoint = ray.GetPoint(distance);
+            transform.position = Vector3.Lerp(this.transform.position, rayPoint, Speed * Time.deltaTime);
+        }
+    }
+}
