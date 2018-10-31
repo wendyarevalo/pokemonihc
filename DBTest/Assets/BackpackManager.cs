35a8dee5e76e051642ab022a6730d6922d3b9eb4
wendyarevalo <33068924+wendyarevalo@users.noreply.github.com>
2018-10-31 01:09:54 -0600

    Atrapar al pony y mandar a quiz

--- DBTest/Assets/BackpackManager.cs
+++ DBTest/Assets/BackpackManager.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BackpackManager : MonoBehaviour
 {
-
+    public bool atrapado;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Pony")
@@ -15,10 +16,34 @@ public class BackpackManager : MonoBehaviour
     }
     IEnumerator CatchPony(GameObject Pony)
     {
-        /*transform.Translate(Vector3.up * 1, Space.World);
+        transform.Translate(Vector3.up * 1, Space.World);
         this.GetComponent<Rigidbody>().useGravity = false;
         this.GetComponent<Rigidbody>().velocity = Vector3.zero;
-        Destroy(Pony.gameObject);*/
-        return null;
+        Destroy(Pony.gameObject);
+        
+
+        yield return new WaitForSeconds(1);
+        transform.Rotate(Vector3.right * 10);
+        yield return new WaitForSeconds(0.4f);
+        transform.Rotate(Vector3.left * 10);
+
+        yield return new WaitForSeconds(1);
+        transform.Rotate(Vector3.right * 10);
+        yield return new WaitForSeconds(0.4f);
+        transform.Rotate(Vector3.left * 10);
+
+        yield return new WaitForSeconds(1);
+        transform.Rotate(Vector3.right * 10);
+        yield return new WaitForSeconds(0.4f);
+        transform.Rotate(Vector3.left * 10);
+
+        atrapado = true;
+
+        if (atrapado)
+        {
+            SceneManager.LoadScene("QuizVF");
+        }
+
+        //return null;
     }
 }
