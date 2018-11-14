857705888cd8c3e53730689d2ced323348177409
wendyarevalo <33068924+wendyarevalo@users.noreply.github.com>
2018-11-14 06:44:26 -0600

    Detalles menores

--- DBTest/Assets/BackpackManager.cs
+++ DBTest/Assets/BackpackManager.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class BackpackManager : MonoBehaviour
 {
     public bool atrapado;
+    System.Random rnd = new System.Random();
+    int aleatorio;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Pony")
@@ -45,7 +48,43 @@ public class BackpackManager : MonoBehaviour
             {
                 ApplicationModel.primeraMat++;
             }
-            SceneManager.LoadScene("SpawningScene");
+            
+
+            aleatorio = rnd.Next(1, 11);
+            Debug.Log(aleatorio);
+            switch (aleatorio) {
+                case 1:
+                    SceneManager.LoadScene("QuizRel");
+                    break;
+                case 2:
+                    SceneManager.LoadScene("QuizVF");
+                    break;
+                case 3:
+                    SceneManager.LoadScene("QuizMult");
+                    break;
+                case 4:
+                    SceneManager.LoadScene("QuizRel");
+                    break;
+                case 5:
+                    SceneManager.LoadScene("QuizVF");
+                    break;
+                case 6:
+                    SceneManager.LoadScene("QuizMult");
+                    break;
+                case 7:
+                    SceneManager.LoadScene("QuizRel");
+                    break;
+                case 8:
+                    SceneManager.LoadScene("QuizVF");
+                    break;
+                case 9:
+                    SceneManager.LoadScene("QuizMult");
+                    break;
+                case 10:
+                    SceneManager.LoadScene("QuizVF");
+                    break;
+            }
+            
         }
 
         //return null;
