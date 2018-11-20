dc322a7ff5a19dc529c433988a9c8ad27af43f62
JudithBl <melissjth@hotmail.com>
2018-11-19 21:17:04 -0600

    version 2 de quiz relacional

--- DBTest/Assets/Drag.cs
+++ DBTest/Assets/Drag.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class Drag : MonoBehaviour, IDragHandler, IBeginDragHandler , IEndDragHandler
+public class Drag : MonoBehaviour
 {
-    public void OnBeginDrag(PointerEventData eventData)
+    private Vector3 offset;
+
+    void OnMouseDown()
     {
-        //throw new System.NotImplementedException();
+
+        offset = gameObject.transform.position -
+            Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1000.0f));
+      //  Debug.Log("on mouse down");
     }
 
-    public void OnDrag(PointerEventData eventData)
+    void OnMouseDrag()
     {
-        transform.position = eventData.position;
+        Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1000.0f);
+        transform.position = Camera.main.ScreenToWorldPoint(newPosition) + offset;
+       // Debug.Log("on mouse drag");
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       // Debug.Log("respuesta 3");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "p1")
+        {
+            Debug.Log("Colisionamos con p1 el objeto "+this.name);
+        }
+        if (collision.gameObject.tag == "p2")
+        {
+            Debug.Log("Colisionamos con p2 el objeto "+this.name);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+       // Debug.Log("salio del obj");
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+      //  Debug.Log("ESTA EN EL OBJ");
     }
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        //throw new System.NotImplementedException();
-    }
 }
--- DBTest/Assets/QuizRL.cs
+++ DBTest/Assets/QuizRL.cs
@@ -6,13 +6,16 @@ using UnityEngine.EventSystems;
 
 public class QuizRL : MonoBehaviour {
 
-    public Image pregunta1;
-    public Image pregunta2;
-    public Image pregunta3;
+   // public Image pregunta1;
+   // public Image pregunta2;
+    /*public Image pregunta3;
     public Image respuesta1;
     public Image respuesta2;
-    public Image respuesta3;
+    public Image respuesta3;*/
+    //public Sprite p1;
+    //public Sprite p2;
     public PreguntaR mypregunta;
+    public TextMesh t1;
     private string UrlPregunta = "http://ihcmon.000webhostapp.com/consultas.php";
     // Use this for initialization
     IEnumerator Start()
@@ -28,33 +31,17 @@ public class QuizRL : MonoBehaviour {
         JsonUtility.FromJsonOverwrite(json, mypregunta);
         Debug.Log(" json " + json);
         Debug.Log("" + mypregunta.respuesta1);
-        pregunta1.GetComponentInChildren<Text>().text = mypregunta.pregunta1;
-        pregunta2.GetComponentInChildren<Text>().text = mypregunta.pregunta2;
-        pregunta3.GetComponentInChildren<Text>().text = mypregunta.pregunta3;
-        respuesta1.GetComponentInChildren<Text>().text = mypregunta.respuesta1;
-        respuesta2.GetComponentInChildren<Text>().text = mypregunta.respuesta2;
-        respuesta3.GetComponentInChildren<Text>().text = mypregunta.respuesta3;
+        /*  pregunta1.GetComponentInChildren<Text>().text = mypregunta.pregunta1;
+          pregunta2.GetComponentInChildren<Text>().text = mypregunta.pregunta2;
+          pregunta3.GetComponentInChildren<Text>().text = mypregunta.pregunta3;
+          respuesta1.GetComponentInChildren<Text>().text = mypregunta.respuesta1;
+          respuesta2.GetComponentInChildren<Text>().text = mypregunta.respuesta2;
+          respuesta3.GetComponentInChildren<Text>().text = mypregunta.respuesta3;*/
+        t1.text = mypregunta.pregunta1;
     }
 
     // Update is called once per frame
     void Update() {
 
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("respuesta 3");
-    }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        Debug.Log(" el obje entro al triger");
-    }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        Debug.Log("salio del obj");
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        Debug.Log("ESTA EN EL OBJ");
-    }
-    
 }
